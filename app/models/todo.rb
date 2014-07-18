8a9fb8691307e32d1b38d2e400c2c33270366555
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-07-18 20:27:43 +0530

    Test

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ActiveRecord::Base
   attr_accessible :name, :status, :completed
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 4 }
 end
--- test/unit/todo_test.rb
+++ test/unit/todo_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save todo without name" do
+    todo = Todo.new
+    assert !todo.save
+  end
+
+  test "should save todo with name" do
+    todo = Todo.new(name: '')
+    assert todo.save
+  end
+
+  test "should save todo with name minimun 4 letters" do
+    todo = Todo.new(name: 'sagar')
+    assert todo.save
+  end
+
+  test "should not save todo with status false" do
+    todo = Todo.new(name: 'sagar', status: false)
+    assert !todo.save
+  end
 end
